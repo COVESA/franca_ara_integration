04fb8a7e5278a81fe93e31f0ff848594cf81257b
Gunnar Andersson <gandersson@genivi.org>
2018-11-19 20:51:31 +0100

    Implement network layer and separate from graphics
    1. Initial implementation of callbacks and processing from updates
    coming from the CommonAPI C++ framework.  This compiles, so in some
    sense it fits together, but I think it's quite far from "communicating"
    yet...
    
    This proposes also a rewrite that takes the thread away from
    ImageSource and creates a networking thread in a separate class.
    
    Rough design:
    
    The networking layer deals with the CommonAPI layer entirely and
    calls functions in ImageSource to trigger updates.  ImageSource
    implements (as before) the Qt-style objects that represent the upddates
    and communicates them to the QML layer using signals.  ImageSource just
    does not have its own thread anymore (its functions are executed from
    the network thread).
    
    From a runtime point of view, nothing changed - there is an equal amount
    of threads.  From a static/structural point of view there is strict
    separation between the datatypes used on CommonAPI (standard C++ only)
    and the Qt World, with the boundary between SomeIpNetworkThread class
    and ImageSource class.
    
    This division causes some extra layers and boiler-plate code as a
    consequence.  We'll see how we like it.
    
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- src/imagesource.cpp
+++ src/imagesource.cpp
@@ -7,6 +7,8 @@
 #define IMAGE_FEED_PATH "/home/mirzak/project/genivi/src/franca_ara_integration/src/feed"
 #define MAX_IMAGE_ID 228
 
+// Internal helpers
+
 // Just a safety check to avoid ridiculous behavior if buggy input
 static int limit_id(int id) {
    // TODO: report if index is outside limits
@@ -16,46 +18,34 @@ static int limit_id(int id) {
 
 static QString image_url(int frameId)
 {
-   return QString("%1/l_image%2.png").arg(IMAGE_FEED_PATH).arg(limit_id(frameId));
+   return QString("%1/l_image%2.png").arg(IMAGE_FEED_PATH).arg(frameId);
 }
 
-static QRect get_bounding_rectangle(void/*todo*/) {
-  return QRect();
+static QRect get_bounding_qrect(BoxDefinition box) {
+   return QRect(box.x, box.y, box.width, box.height);
+};
+
+static Drawn_Lane_Boundary_t get_bounding_lines(void/*todo*/) {
+   return Drawn_Lane_Boundary_t(QLine(),QLine());
 }
 
-// Could be a class but hey...
-typedef std::pair<QLine, QLine> lane_boundaries_t;
 
-static lane_boundaries_t get_bounding_lines(void/*todo*/) {
-   return lane_boundaries_t(QLine(),QLine());
+// Public functions called by networking class/thread:
+
+void ImageSource::newFrameId(int frameID) {
+      QImageReader reader(image_url(limit_id(frameID)));
+      emit imageReady(reader.read()); // Signal to QML
 }
 
-void ImageSource::run()
-{
-   uint8_t imagecount = 0;
-   QElapsedTimer timer;
-   timer.start();
-
-   while (true) {
-      // Here the loop should be driven by CommonAPI networking updates
-      // The attribute Vehicles of type ListOfVehicles will get updates
-      // which include FrameID and a list of identified vehicles
-
-      // TODO: Blocking read on attribute update?
-
-      // Signal graphics thread to update camera image
-      QImageReader reader(image_url(imagecount++));
-      emit imageReady(reader.read());
-
-      // Update identified objects
-      while(0) // TODO: For each vehicle
-         // Signal the QML graphics thread to draw a new box around vehicle
-         // todo: different colors?
-         emit vehicle_identified(get_bounding_rectangle(/*...*/));
-
-         // Signal QML graphics to draw lane identifications
-         lane_boundaries_t lines = get_bounding_lines(/*...*/);
-         emit lane_identified(lines.first, lines.second);
-      }
+void ImageSource::newVehicleIdentification (const BoxDefinition &box) {
+   QRect rect = get_bounding_qrect(box);
+   emit vehicle_identified(rect); // Signal to QML
 }
 
+void ImageSource::newLaneIdentification (const LaneLineDefinition &left, const LaneLineDefinition &right) {
+   Q_UNUSED(left);
+   Q_UNUSED(right);
+
+   Drawn_Lane_Boundary_t lines = get_bounding_lines(/*...*/);
+   emit lane_identified(lines.first, lines.second);  // Signal to QML
+}
--- src/imagesource.h
+++ src/imagesource.h
@@ -3,12 +3,25 @@
 
 #include <QThread>
 #include <QImage>
+#include "SomeIpNetworkThreadTypes.h"
 
-class ImageSource : public QThread
+// Types used in communication between image source class (C++)
+// and the QML thread. (All other signals use primitive Qt types
+// like QRect etc.)
+typedef std::pair<QLine, QLine> Drawn_Lane_Boundary_t;
+
+class ImageSource : public QObject
 {
     Q_OBJECT
 
-    void run() override;
+// These functions are called explicitly (they are not slots that can be
+// bound to any signal), from the network thread.  The functions take
+// "non-Qt" data and construct an appropriate Qt object and send it by
+// signal to the QML thread.
+public:
+    void newFrameId(int frameID);
+    void newVehicleIdentification (const BoxDefinition &box);
+    void newLaneIdentification (const LaneLineDefinition &left, const LaneLineDefinition &right);
 
 Q_SIGNALS:
     void imageReady(const QImage &s);
