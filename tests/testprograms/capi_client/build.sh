486f6399ecfb0d7907f4b5a793b49bf2a78bb899
Gunnar Andersson <gandersson@genivi.org>
2018-12-05 12:53:13 +0100

    Use CC variable in build scripts
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- tests/testprograms/capi_client/build.sh
+++ tests/testprograms/capi_client/build.sh
@@ -1,12 +1,17 @@
-#!/bin/sh -x
+#!/bin/sh
 
-g++ -o capi_client -I src-gen -I/usr/local/include/CommonAPI-3.1 \
+cd "$(dirname "$0")"
+
+# Use the CC variable if it was set in an SDK
+if [ -z "$CC" ] ; then
+  CC=g++
+fi
+
+set -x
+$CC -o capi_client -std=c++14 -I src-gen -I/usr/local/include/CommonAPI-3.1 \
   main.cpp \
     src-gen/v1/genivi/aasr/showcase/*.cpp \
     -lCommonAPI-SomeIP  \
     -lCommonAPI \
     -lpthread \
     -lstdc++
-
-
-  #SomeIpNetworkThread.cpp \
