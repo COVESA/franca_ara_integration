ad58902739ddaa76ffbd7a65528bb102452e874d
Gunnar Andersson <gandersson@genivi.org>
2018-12-05 10:53:42 +0100

    simple_capi_server: Added (new test program)
    Add testprogram for Simple CommonAPI server.  By using a CommonAPI based
    server (in addition to client) we can easier study what the
    serialization looks like, to compare it to the AUTOSAR way.
    
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

---
+++ tests/testprograms/simple_capi_server/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd $(dirname $0)
+
+test_file_exists() {
+  [ -f "$1" ] || echo "Missing file $1 !"
+}
+export COMMONAPI_CONFIG="$(readlink -f ../../../conf/commonapi.ini)"
+#export COMMONAPI_SOMEIP_CONFIG="$(readlink -f ../../../conf/commonapi-someip.ini)"
+export VSOMEIP_CONFIGURATION="$(readlink -f ../../../conf/vsomeip_ecu1.json)"
+test_file_exists $COMMONAPI_CONFIG
+#test_file_exists $COMMONAPI_SOMEIP_CONFIG
+test_file_exists $VSOMEIP_CONFIGURATION
+
+export VSOMEIP_CLIENTSIDELOGGING=true
+
+echo Starting simple_capi_server
+echo
+echo Start vsomeipd and vsomeip/client, but not vsomeip/server
+
+LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64 ./simple_capi_server
