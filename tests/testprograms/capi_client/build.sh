ddfc12d6c4ad19d1082da248ce0f2af9ded57862
Gunnar Andersson <gandersson@genivi.org>
2018-12-11 20:13:53 +0100

    Use unique name for GDP binaries
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- tests/testprograms/capi_client/build.sh
+++ tests/testprograms/capi_client/build.sh
@@ -2,13 +2,21 @@
 
 cd "$(dirname "$0")"
 
+# This depends on where you installed the files
+# but should not matter in the final SDK/Image builds
+INCDIR="-I/usr/local/include/CommonAPI-3.1"
+
 # Use the CC variable if it was set in an SDK
 if [ -z "$CC" ] ; then
   CC=g++
+  BINARY=capi_client
+else
+  # Assume this means we are building for target
+  BINARY=capi_client_gdp
 fi
 
 set -x
-$CC -o capi_client -std=c++14 -I src-gen -I/usr/local/include/CommonAPI-3.1 \
+$CC -o $BINARY -std=c++14 -I src-gen $INCDIR \
   main.cpp \
     src-gen/v1/genivi/aasr/showcase/*.cpp \
     -lCommonAPI-SomeIP  \
