c92545106c4afeeef849f6daa64c9318398a2986
Mirza Krak <mirza.krak@northern.tech>
2018-12-14 16:19:53 +0100

    main: remove recognition model decleration
    This is handled by the SomeIpNetworkThread
    
    Signed-off-by: Mirza Krak <mirza.krak@northern.tech>

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,9 @@
 #include <QQmlContext>
 
 #include "SomeIpNetworkThread.h"
-#include "imageprovider.h"
 
 #include <dlt.h>
-
 #include "dltloggername.h"
-#include "recognitionmodel.h"
 
 DLT_DECLARE_CONTEXT(DLT_FRA_ARA_CONTEXT)
 
@@ -28,31 +25,9 @@ int main(int argc, char *argv[])
     setenv("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1", 1);
 
     SomeIpNetworkThread *network_thread = new SomeIpNetworkThread();
+
+    // contains QML connections which must run before view.setSource
     network_thread->connections(view);
-
-    BoxDefinition b;
-    b.height = 100;;
-    b.width = 100;
-    b.x = 100;
-    b.y = 100;
-    b.color = "Red";
-
-
-    LaneLineDefinition left;
-    left.lower_x = 0;
-    left.lower_y = 12;
-    left.upper_x = 50;
-    left.upper_y = 60;
-
-    LaneLineDefinition right;
-    right.lower_x = 1;
-    right.lower_y = 2;
-    right.upper_x = 3;
-    right.upper_y = 4;
-
-    RecognitionModel *model = new RecognitionModel(200, b, LaneDefinition_t(left, right));
-    QQmlContext *ctxt = view.rootContext();
-    ctxt->setContextProperty("recognitionModel", model);
 
     view.engine()->addImportPath(QCoreApplication::applicationDirPath() +
                                  "/imports");
