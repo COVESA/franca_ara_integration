2ed1992170a8cb1cb4a16a8ffafaf287b11c6f6f
Gunnar Andersson <gandersson@genivi.org>
2018-12-14 14:07:53 +0100

    Target wrapper script to run franca-ara
    A wrapper similar to run_capi_client_gdp.  This starts vsomeipd if it is
    not started and it sets the environment variable that is needed for
    correct VSOMEIP configuration.
    
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

---
+++ deploy/usr/local/bin/run_franca_ara.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+
+export VSOMEIP_CONFIGURATION="$(readlink -f /usr/local/share/franca_ara/conf/vsomeip_ecu1.json)"
+export VSOMEIP_CLIENTSIDELOGGING=true
+
+[ -z "$VSOMEIP_CONFIGURATION" ] && { echo "ERROR: Did not find VSOMEIP_CONFIGURATION file as expected!" ; exit 1 ; }
+
+# Run vsomeipd if it's not in process list already
+ps aux | grep -q [v]someipd 
+if [ $? -eq 0 ] ; then
+  echo 
+  echo VSOMEIPD ALREADY RUNNING -- SKIPPED
+  echo
+else
+  echo -------------------------------
+  echo STARTING VSOMEIPD
+  echo -------------------------------
+  vsomeipd &
+fi
+
+  echo -------------------------------
+  echo STARTING FRANCA-ARA
+  echo -------------------------------
+
+cd "$(dirname "$0")"
+./franca-ara
+
