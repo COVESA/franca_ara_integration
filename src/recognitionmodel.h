6ccf41017869c30836cd17c280eb91b7116c8ffa
Gunnar Andersson <gandersson@genivi.org>
2018-12-14 13:29:08 +0100

    Miscellaneous work in progress
    Don't have time to clean, rebase, & split this commit now.  Maybe later.

--- src/recognitionmodel.h
+++ src/recognitionmodel.h
@@ -15,9 +15,9 @@ class RecognitionModel : public QObject
 {
     Q_OBJECT
 
-    Q_PROPERTY(int id READ id WRITE setId NOTIFY idChanged)
+    //Q_PROPERTY(int id READ id WRITE setId NOTIFY idChanged)
   //  Q_PROPERTY(BoxDefinition box READ box WRITE setBox NOTIFY boxChanged)
-    Q_PROPERTY(LaneDefinition_t lanes READ lanes WRITE setLanes NOTIFY lanesChanged)
+    Q_PROPERTY(LaneDefinition_t lanes READ lanes NOTIFY lanesChanged)
 
     Q_PROPERTY(int boxX READ boxX NOTIFY boxChanged);
     Q_PROPERTY(int boxY READ boxY NOTIFY boxChanged);
@@ -41,10 +41,10 @@ public:
     QColor boxColor() const {return QColor(m_box.color.c_str()); };
 
     LaneDefinition_t lanes() const;
-    void setLanes(const LaneDefinition_t &color);
+    void newLaneIdentification(const LaneDefinition_t &color);
 
-    int id() const;
-    void setId(const int &id);
+//    int id() const;
+//    void setId(const int &id);
 
 signals:
     void boxChanged();
