bd86fbcd13c1813403f500dacbe9f31750192cd4
Gunnar Andersson <gandersson@genivi.org>
2019-01-02 14:31:20 +0100

    Parameterize host setup script (minor)

--- conf/devhost/set_host_dev_ip.sh
+++ conf/devhost/set_host_dev_ip.sh
@@ -1,16 +1,20 @@
 #!/bin/sh -x
-sudo ip a a 192.168.100.42/24 dev enp0s20f0u1
+
+DEV=enp0s20f0u2
+sudo ip a a 192.168.100.42/24 dev $DEV
 
 # Add route if needed
 route | grep 192.168.100.0
 if [ $? -ne 0 ] ; then
-   sudo route add -net 192.168.100.0/24 dev enp0s20f0u1
+   sudo route add -net 192.168.100.0/24 dev $DEV
 fi
 
 route | grep 224.0.0.0
 if [ $? -ne 0 ] ; then
-   sudo route add -net 224.0.0.0/8 dev enp0s20f0u1
+   sudo route add -net 224.0.0.0/8 dev $DEV
 fi
 
 ip a
 ping 192.168.100.10
+
+
